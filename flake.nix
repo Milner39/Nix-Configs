4f632c9f977804947ed901c96a97199354639244
Finn Milner <91906877+Milner39@users.noreply.github.com>
2025-07-30 20:50:11 +0100

    refactor

--- flake.nix
+++ flake.nix
@@ -1,37 +1,68 @@
 {
   inputs = {
+    # === Essentials ===
+
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # === Essentials ===
+
   };
 
   outputs = {
+    self,
     nixpkgs,
     home-manager,
     ...
-  } @ inputs: {
-  
+  } @ inputs: let
+
+    # Extend lib with lib.custom
+    lib = nixpkgs.lib.extend (self: super: {
+      custom = {};  # import ./lib { inherit (nixpkgs) lib; };
+    });
+
+    baseSpecialArgs = { inherit
+      inputs
+      lib;
+    };
+
+  in {
     homeConfigurations = {
-      default = let
-        username = "FinnM";
+      # === finnm@FM-PC-NIXOS ===
+
+      "finnm@FM-PC-NIXOS" = let
+        username = "finnm";
 
         system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        extraSpecialArgs = { inherit
-          username
-          inputs;
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
         };
+
+        extraSpecialArgs = baseSpecialArgs // { inherit
+          username;
+        };
+
       in home-manager.lib.homeManagerConfiguration {
-        inherit pkgs extraSpecialArgs;
+        inherit system pkgs extraSpecialArgs;
 
         modules = [
-          ./homes/default/home.nix
+          {
+            home = {
+              username = username;
+              homeDirectory = "/home/${username}";
+            };
+          }
+
+          ./homes/FM-PC-NIXOS/finnm/home.nix
         ];
       };
+
+      # === finnm@FM-PC-NIXOS ===
     };
   };
 }
