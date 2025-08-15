423199519a4df04695ccc487c3e21e14da3a4e21
Milner39 <91906877+Milner39@users.noreply.github.com>
2025-08-15 20:27:07 +0100

    feat: add font

--- homes/FM-PC-NIXOS/finnm/home.nix
+++ homes/FM-PC-NIXOS/finnm/home.nix
@@ -41,6 +41,8 @@ in
 
   # Packages
   home.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
