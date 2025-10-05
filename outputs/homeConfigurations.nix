37e4ef72c7a65890688152cfd9fd3ca198358c63
Milner39 <91906877+Milner39@users.noreply.github.com>
2025-10-05 19:04:49 +0100

    refactor: neither of these work

--- outputs/homeConfigurations.nix
+++ outputs/homeConfigurations.nix
@@ -1,6 +1,9 @@
 {
   homeConfigurations = {
-    dev = (builtins.getFlake "path:../flakes/homes/dev")
+    # dev = (builtins.getFlake ("path:" + toString ../flakes/homes/dev))
+    #   .outputs.homeConfigurations.dev;
+
+    dev = (import ../flakes/homes/dev/flake.nix)
       .outputs.homeConfigurations.dev;
   };
 }
