bd5025001247116e1108a2d8b3afc50be6433596
Milner39 <91906877+Milner39@users.noreply.github.com>
2025-10-05 20:47:48 +0100

    chore: try again later

--- outputs/homeConfigurations.nix
+++ outputs/homeConfigurations.nix
@@ -3,7 +3,7 @@
     # dev = (builtins.getFlake ("path:" + toString ../flakes/homes/dev))
     #   .outputs.homeConfigurations.dev;
 
-    dev = (import ../flakes/homes/dev/flake.nix)
-      .outputs.homeConfigurations.dev;
+    # dev = (import ../flakes/homes/dev/flake.nix)
+    #   .outputs.homeConfigurations.dev;
   };
 }
