b35f3bb68b3bef2a52a0b2fc33b897b3a64ff1dd
Milner39 <91906877+Milner39@users.noreply.github.com>
2026-08-19 22:09:43 +0100

    fix: broken sddm

--- modules/nixos/gui/display-manager/sddm/default.nix
+++ modules/nixos/gui/display-manager/sddm/default.nix
@@ -29,9 +29,14 @@ in
       enable = true;
       package = pkg;
 
-      # Enable support for Wayland (Doesn't mean Wayland must be used)
-      wayland.enable = true;
+      # # Enable support for Wayland
+      # wayland.enable = true;
+      # wayland.compositor = "weston";
     };
+
+    # NOTE: As of nixpkgs 26.05, required for mouse input (annoying)
+    # Uncomment lines above when fix exists
+    services.xserver.enable = lib.mkForce true;
   };
   # === Config ===
 }
